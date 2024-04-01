8ac8f5fb642127b5a791930ef53b41688acb7554
dongrep <dongre.p@northeastern.edu>
2024-04-01 16:28:30 -0400

    Added code for creating MIG

--- compute/main.tf
+++ compute/main.tf
@@ -84,10 +84,103 @@ resource "google_dns_record_set" "default" {
   type         = "A"
   ttl          = 300
   rrdatas = [
-    google_compute_instance.webapp-instance.network_interface[0].access_config[0].nat_ip
+    google_compute_instance.webapp-instance.network_interface.0.network_ip
   ]
 }
 
+resource "google_compute_instance_template" "lb-instance-template" {
+  name         = "webapp-instance-${random_id.instance_id.hex}"
+  machine_type = "e2-medium"
+
+  service_account {
+    scopes = ["cloud-platform"]
+    email  = google_service_account.service_account.email
+  }
+
+  disk {
+    source_image = "projects/${var.project_id}/global/images/${var.image_name}"
+    disk_size_gb = 100
+    disk_type    = "pd-balanced"
+  }
+
+  network_interface {
+    network    = var.network
+    subnetwork = var.subnet
+  }
+
+  tags = ["webapp-subnet"]
+
+  metadata_startup_script = <<-EOF
+    touch .env
+    echo POSTGRESQL_DB=${var.db_name} >> .env
+    echo POSTGRESQL_USER=${var.db_user} >> .env
+    echo POSTGRESQL_PASSWORD=${var.db_password} >> .env
+    echo POSTGRESQL_HOST=${var.private_ip} >> .env
+    echo PORT=6969 >> .env
+
+    sudo mv .env /opt/webapp/.env
+    EOF
+}
+
+
+resource "google_compute_health_check" "http-health-check" {
+  name = "http-health-check-${random_id.manager_id.hex}"
+
+  timeout_sec         = 10
+  check_interval_sec  = 30
+  healthy_threshold   = 2
+  unhealthy_threshold = 3
+
+  http_health_check {
+    port         = "6969"
+    request_path = "/healthz"
+  }
+}
+
+resource "google_compute_autoscaler" "default" {
+  name   = "webapp-autoscaler-${random_id.manager_id.hex}"
+  target = google_compute_instance_group_manager.default.id
+  zone   = var.zone
+
+  autoscaling_policy {
+    max_replicas    = 2
+    min_replicas    = 1
+    cooldown_period = 60
+
+    cpu_utilization {
+      target = 0.05
+    }
+  }
+}
+
+resource "random_id" "manager_id" {
+  byte_length = 4
+}
+
+resource "google_compute_instance_group_manager" "default" {
+  name               = "webapp-instance-group-manager-${random_id.manager_id.hex}"
+  base_instance_name = "webapp-instance"
+  zone               = var.zone
+
+  depends_on = [
+    google_compute_instance_template.lb-instance-template
+  ]
+
+  version {
+    instance_template = google_compute_instance_template.lb-instance-template.self_link
+  }
+
+  named_port {
+    name = "http"
+    port = 6969
+  }
+
+  auto_healing_policies {
+    health_check      = google_compute_health_check.http-health-check.id
+    initial_delay_sec = 30
+  }
+}
+
 output "webapp_private_ip" {
-  value = google_compute_instance.webapp-instance.network_interface[0].network_ip
+  value = google_compute_instance.webapp-instance.network_interface.0.network_ip
 }
