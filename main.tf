2dced7966f7e4617b0c0c2df7bab38e583f29fee
dongrep <dongre.p@northeastern.edu>
2024-04-01 16:28:30 -0400

    Added code for creating MIG

--- main.tf
+++ main.tf
@@ -29,12 +29,12 @@ module "subnet" {
   vpc_network_id       = module.vpc.vpc_network_id
 }
 
-module "routes" {
-  source         = "./routes"
-  project_id     = var.project_id
-  dest_range     = var.dest_range
-  vpc_network_id = module.vpc.vpc_network_id
-}
+# module "routes" {
+#   source         = "./routes"
+#   project_id     = var.project_id
+#   dest_range     = var.dest_range
+#   vpc_network_id = module.vpc.vpc_network_id
+# }
 
 module "sql_db" {
   source            = "./sql_db"
@@ -60,16 +60,16 @@ module "compute" {
   private_ip  = module.sql_db.host_ip
 }
 
-module "cloudfunction" {
-  source         = "./cloudfunction"
-  project_id     = var.project_id
-  db_name        = module.sql_db.db_name
-  db_user        = module.sql_db.db_user
-  db_password    = module.sql_db.db_password
-  private_ip     = module.sql_db.host_ip
-  vpc_network_id = module.vpc.vpc_network_id
-  vm_ip          = module.compute.webapp_private_ip
-}
+# module "cloudfunction" {
+#   source         = "./cloudfunction"
+#   project_id     = var.project_id
+#   db_name        = module.sql_db.db_name
+#   db_user        = module.sql_db.db_user
+#   db_password    = module.sql_db.db_password
+#   private_ip     = module.sql_db.host_ip
+#   vpc_network_id = module.vpc.vpc_network_id
+#   vm_ip          = module.compute.webapp_private_ip
+# }
 
 module "firewall" {
   source     = "./firewall"
